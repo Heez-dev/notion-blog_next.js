f54890fb09fa4cb65ca334675982464806d3a7ea
heez.dev <heez.dev@gmail.com>
2025-11-13 20:26:33 +0900

    feat: 블로그 상세 목차 UI 추가

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="bg-background sticky top-0 z-50 border-b">
-      <div className="container flex h-14 items-center">
+      <div className="container flex h-(--header-height) items-center">
         <a href="#" className="text-xl font-semibold">
           <span className="font-bold">Heez.dev</span>
         </a>
